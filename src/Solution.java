46b5d74335982f1b9336ecf4ea6f3811280f2194
sachin jamthe <>
2020-03-29 12:07:52 -0500

    Completed Merge meetings code

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
 
     public static void main(String[] args){
         //My first programming class
         //My second comment
         System.out.println("My Interview Cake solutions");
+
+        MergeMeetings m = new MergeMeetings();
+
+        List<MergeMeetings.Meeting> meetings = Arrays.asList(
+                new MergeMeetings.Meeting(0, 1), new MergeMeetings.Meeting(3, 5), new MergeMeetings.Meeting(4, 8),
+                new MergeMeetings.Meeting(10, 12), new MergeMeetings.Meeting(9, 10)
+        );
+
+        System.out.println(m.mergeRanges(meetings).toString());
+
     }
 
 }
