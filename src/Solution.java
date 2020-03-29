561661e3c6be2f2423bff49350a2fac40931e2ec
sachin jamthe <>
2020-03-29 10:07:54 -0500

    Added message

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,7 @@
 public class Solution {
 
     public static void main(String[] args){
-        //
+        //My first programming class
         System.out.println("My Interview Cake solutions");
     }
 
